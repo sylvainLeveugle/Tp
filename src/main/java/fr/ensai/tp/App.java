774b4d12210fd7d260b5dae89d0a312689fe4c80
sylvainLeveugle <sylvain.leveugle@hotmail.fr>
2015-09-23 14:46:50 +0200

    new

---
+++ src/main/java/fr/ensai/tp/App.java
@@ -0,0 +1,14 @@
+package fr.ensai.tp;
+
+/**
+ * Hello world! 
+ * 
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+/* Ceci est un commentaire*/
